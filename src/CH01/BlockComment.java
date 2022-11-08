e1e270b2f6f34bd430ab20f4dc22a4bc5d8a7bf8
NOOBNUBY <noob77@gmail.com>
2022-11-08 23:19:13 +0900

    ch1

---
+++ src/CH01/BlockComment.java
@@ -0,0 +1,11 @@
+package CH01;
+
+public class BlockComment {
+    public static void main(String[] args) {
+        /*
+        파일이름 : BlockComment.java
+        작 성 자: 홍길동
+        작 성 일: 20** ** **
+        */
+    }
+}
---
+++ src/CH01/LineComment.java
@@ -0,0 +1,7 @@
+package CH01;
+
+public class LineComment {
+    public static void main(String[] args) {
+        //행 단위 주석
+    }
+}
